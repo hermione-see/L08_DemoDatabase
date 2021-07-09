4bb7623eb2e6f5dcba9064a42fb5e18593dc2c7e
hermione-see <20019634@myrp.edu.sg>
2021-07-09 11:45:50 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20019634/l08_demodatabase/MainActivity.java
@@ -0,0 +1,32 @@
+package sg.edu.rp.c346.id20019634.l08_demodatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnInsert = findViewById(R.id.buttonInsert);
+        btnInsert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertTask("Submit RJ", "25 Apr 2021");
+
+            }
+        });
+    }
+}
